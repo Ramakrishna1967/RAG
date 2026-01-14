d89ca92354052f7d77f5f82a601aee3806fd1142
Rama krishna <batem6050@gmail.com>
2026-01-15 01:28:04 +0530

    Update Basic rag.py

--- Basic rag.py
+++ Basic rag.py
@@ -1,4 +1,17 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
+loader = PyPDFLoader("your_internship_data.pdf")
+raw_data = loader.load()
 
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, 
+    chunk_overlap=150
+)
+chunks = text_splitter.split_documents(raw_data)
+
+print(f"Block 1 Complete: Created {len(chunks)} chunks.")
 
